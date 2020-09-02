36df6fb202a498e0707d842ff9617d0ec0a45bac
kanasaki15 <kassi_0123@kassi-hp.info>
2020-09-02 19:42:03 +0900

    Initial commit

---
+++ src/main/java/xyz/n7mn/dev/papermc/dpcpluginbypaper/DPCPluginByPaper.java
@@ -0,0 +1,17 @@
+package xyz.n7mn.dev.papermc.dpcpluginbypaper;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DPCPluginByPaper extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
